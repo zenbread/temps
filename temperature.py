d79d2bb202ddac488f405b752d5a69020dc56bb6
zenBread <nmcamp87@gmail.com>
2019-02-20 14:35:43 -0500

    did homework

---
+++ temperature.py
@@ -0,0 +1,36 @@
+
+class Temperature():
+    def __init__(self, C = None, F = None, K = 0, D = None):
+        self.C = C
+        self.F = F
+        self.K = K
+        self.D = D
+        if C != None or F != None or K != None or D != None:
+            self._GetTemps()
+            self._RoundTemps()
+
+    def _GetTemps(self):
+        if self.C != None:
+            self.C = float(self.C)
+            self.D = (100 - self.C) * float(3) / float(2)
+            self.F = 212 - self.D * float(6) / float(5)
+            self.K = 373.15 - self.D * float(2) / float(3)
+        elif self.F != None:
+            self.F = float(self.F)
+            self.D = (212 - self.F) * float(5) / float(6)
+            self.K = 373.15 - self.D * float(2) / float(3)
+            self.C = 100 - self.D * float(2) / float(3)
+        elif self.K != None:
+            self.K =float(self.K)
+            self.D = (373.15 - self.K) * float(3) / float(2)
+            self.C = 100 - self.D * float(2) / float(3)
+            self.F = 212 - self.D * float(6) / float(5)
+        elif self.D != None:
+            self.D = float(self.D)
+            self.C = 100 - self.D * float(2) / float(3)
+            self.F = 212 - self.D * float(6) / float(5)
+            self.K = 373.15 - self.D * float(2) / float(3)
+
+    def _RoundTemps(self):
+        for x in self.__dict__.keys():
+            self.__dict__[x] = round(self.__dict__[x], 2)
---
+++ tests/test.py
@@ -0,0 +1,28 @@
+import unittest
+import temperature
+
+class TestTemp(unittest.TestCase):
+    def test_create(self):
+        self.assertIsNotNone(temperature.Temperature())
+
+    def test_Cel(self):
+        p = temperature.Temperature(C=1)
+        self.assertIsNotNone(p.C)
+
+    def test_Far(self):
+        p = temperature.Temperature(F=1)
+        self.assertEqual(p.F, 1)
+
+    def test_Kel(self):
+        p = temperature.Temperature(K=1)
+        self.assertEqual(p.K, 1)
+
+    def test_Del(self):
+        p = temperature.Temperature(D=1)
+        self.assertEqual(p.D, 1)
+
+    def test_CelAll(self):
+        p = temperature.Temperature(C=1)
+        self.assertEqual(p.D, 148.5)
+        self.assertEqual(p.K, 274.15)
+        self.assertEqual(p.F, 33.8)
